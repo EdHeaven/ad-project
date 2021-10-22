82fb4b17f667cf17d73c2bc1fd6a163a3ae34540
EdHeaven <ablyazizov.e.i20@gmail.com>
2021-10-22 10:17:44 +0300

    2.2

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn  text><v-icon left>mdi-help</v-icon>QnA</v-btn> 
+       <v-btn  text><v-icon left>mdi-login-variant</v-icon>Login in</v-btn> 
+       <v-btn  text><v-icon left>mdi-account-arrow-right-outline</v-icon>Sign up</v-btn>       
     </v-toolbar-items>
   </v-app-bar>
   <v-main>
