42ebbaf07f25aac4bc7a04c615a61adbe1a50053
Edem Ablyazizov <ablyazizov.e.i20@gmail.com>
2021-11-28 15:08:28 +0300

    4.1_4.3

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,22 +1,22 @@
 <template> 
-	<v-container fluid fill-height>
-		<v-layout align-center justify-center> 
-		<v-flex xs12 sm8 md8>
-			<v-card class="elevetion-12">
-				<v-toolbar dark color="primary">
-					Login in
-				</v-toolbar>
-				<v-card-text>
-					<v-form v-model="valid" ref="form" validation>
-						<v-text-field
-						prepend-icon="mdi-account"
-						name="email"
-						label="Email"
-						type="email"
-						v-model="email"
-						:rules="emailRules"
-						></v-text-field>
-						<v-text-field
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center> 
+    <v-flex xs12 sm8 md8>
+      <v-card class="elevetion-12">
+        <v-toolbar dark color="primary">
+          Log in
+        </v-toolbar>
+        <v-card-text>
+          <v-form v-model="valid" ref="form" validation>
+            <v-text-field
+            prepend-icon="mdi-account"
+            name="email"
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            ></v-text-field> 
+                        <v-text-field
                         prepend-icon="mdi-lock" 
                         name="password" 
                         label="Password" 
@@ -25,47 +25,47 @@
                         :rules="passwordRules"
                         >
                         </v-text-field>
-					</v-form>
-				</v-card-text>
-				<v-card-actions>
-					<v-spacer></v-spacer>
-					<v-btn 
-					color="primary"
-					@click="onSubmit"
-					:disabled="!valid"
-					>Login</v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-flex>
-		</v-layout> 
-	</v-container>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn 
+          color="primary"
+          @click="onSubmit"
+          :disabled="!valid"
+          >Log in</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-	data () { 
-		return {
-			valid: false,
-			email: "",
-			password: "",
-			emailRules: [
-			v => !!v || 'E-mail is required',
-			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-			],
-			passwordRules: [
-			v => !!v || 'Password is required',
-			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
-			]
-		} 	
-	},
-		methods: {
-		onSubmit(){
-			const user = {
-				email: this.email,
-				password: this.password
-			}
-			console.log(user)
-		}
-	}
-} 
+  data () { 
+    return {
+      valid: false,
+      email: "",
+      password: "",
+      emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+      ]
+    } 	
+  },
+    methods: {
+    onSubmit(){
+      const user = {
+        email: this.email,
+        password: this.password
+      }
+      console.log(user)
+    }
+  }
+}
 </script>
