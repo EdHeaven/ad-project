3ff002fa35ea4f6cf57292e9f8efd46565656b3e
Edem Ablyazizov <ablyazizov.e.i20@gmail.com>
2021-12-02 19:09:09 +0300

    12.1_12.3

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,20 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyCQGwEbXk_WX-dvJ5IsvemcDdSfBYrr2mw",
+      authDomain: "ad-pro-cf911.firebaseapp.com",
+      projectId: "ad-pro-cf911",
+      storageBucket: "ad-pro-cf911.appspot.com",
+      messagingSenderId: "1084782525129",
+      appId: "1:1084782525129:web:3d0ae9bdcff679f6cd2b18",
+      measurementId: "G-89LWK61H52"
+    };
+  // Initialize Firebase
+    fb.initializeApp(firebaseConfig);
+    fb.analytics();
+  }
+
 }).$mount('#app')
